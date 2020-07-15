75410776595fb8da703ddb573eb9cbd1d8131be9
Antonio Vivace <avivace4@gmail.com>
2020-07-15 16:16:47 +0200

    Fix L1 vs L2 comparison plot

--- figures/src/loss_functions.py
+++ figures/src/loss_functions.py
@@ -5,53 +5,40 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["font.serif"] = "Adobe Caslon Pro"
 plt.rcParams["axes.titlesize"] = "larger"
+plt.rcParams["mathtext.fontset"] = "stix"
 
-def mse(true, pred):
-    """
-    true: array of true values    
-    pred: array of predicted values
-    
-    returns: mean square error loss
-    """
-    
-    return np.sum((true - pred)**2)
-
-def mae(true, pred):
-    """
-    true: array of true values    
-    pred: array of predicted values
-    
-    returns: mean absolute error loss
-    """
-    
-    return np.sum(np.abs(true - pred))
-
+xaxis_label = 'Prediction Error ($y - \hat{y}$)'
 fig, ax1 = plt.subplots(1,1, figsize = (7,5))
 
 # array of same target value 10000 times
 target = np.repeat(100, 10000) 
 pred = np.arange(-10000,10000, 2)
 
-loss_mse = [mse(target[i], pred[i]) for i in range(len(pred))]
-loss_mae = [mae(target[i], pred[i]) for i in range(len(pred))]
+x = np.arange(-1., 1.01, 0.01)
+
+mae = np.abs(x)
+mse = np.power(x, 2)
+
+#loss_mse = [mse(target[i], pred[i]) for i in range(len(pred))]
+#loss_mae = [mae(target[i], pred[i]) for i in range(len(pred))]
 
 color = 'tab:red'
-mae, = ax1.plot(pred, loss_mae, color=color, label='L1')
-ax1.set_xlabel('Predictions')
-ax1.set_ylabel('L1', color=color)
-ax1.tick_params(axis='y', labelcolor=color)
+mseplot, = ax1.plot(x, mse, color=color, label='MSE (L1)')
+maeplot, = ax1.plot(x, mae, color="tab:blue", label="MAE (L2)")
+ax1.set_xlabel(xaxis_label)
+ax1.set_ylabel('Loss')
 
-ax2 = ax1.twinx()
+#ax2 = ax1.twinx()
 
 color = 'tab:blue'
-ax2.set_ylabel('L2', color=color)
-ax2.tick_params(axis='y', labelcolor=color)
-mse, = ax2.plot(pred, loss_mse, color=color, label='L2')
+#ax2.set_ylabel('L2', color=color)
+#ax2.tick_params(axis='y', labelcolor=color)
+#mse, = ax1.plot(pred, loss_mse, color=color, label='L2')
 
 #ax1.set_title("MSE (L1) vs MAE (L2)")
 
 #fig.tight_layout()
-plt.legend(handles=[mse, mae])
+plt.legend(handles=[mseplot, maeplot])
 print("Exported loss_L1_L2.svg")
 plt.savefig(fname="../loss_L1_L2.svg")
 
@@ -80,7 +67,7 @@ losses_huber = [[sm_mae(target[i], pred[i], q) for i in range(len(pred))] for q 
 # plot 
 for i in range(len(delta)):
     ax1.plot(pred, losses_huber[i], label = delta[i])
-ax1.set_xlabel('Predictions')
+ax1.set_xlabel(xaxis_label)
 ax1.set_ylabel('Loss')
 #ax1.set_title("Huber Loss/ Smooth MAE Loss")
 ax1.legend(title="$\delta$ values")
@@ -105,7 +92,7 @@ pred = np.arange(-10,10, 0.02)
 loss_logcosh = [logcosh(target[i], pred[i]) for i in range(len(pred))]
 
 ax1.plot(pred, loss_logcosh)
-ax1.set_xlabel('Predictions')
+ax1.set_xlabel(xaxis_label)
 ax1.set_ylabel('Loss')
 print("Exported loss_logcosh.svg")
 plt.savefig(fname="../loss_logcosh.svg")
@@ -132,7 +119,7 @@ losses_quan = [[quan(target[i], pred[i], q) for i in range(len(pred))] for q in 
 # plot 
 for i in range(len(quantiles)):
     ax1.plot(pred, losses_quan[i], label = quantiles[i])
-ax1.set_xlabel('Predictions')
+ax1.set_xlabel(xaxis_label)
 ax1.set_ylabel('Loss')
 ax1.legend(title="Quantiles ($\gamma$)")
 print("Exported loss_quant.svg")
