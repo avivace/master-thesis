c4cc5e6d57b8b8e0d9dc17b01fe9fda6aa8a740f
Antonio Vivace <avivace4@gmail.com>
2020-07-15 03:38:54 +0200

    Plot Quantile and Cosh Loss Functions

--- figures/src/loss_functions.py
+++ figures/src/loss_functions.py
@@ -48,7 +48,7 @@ ax2.set_ylabel('L2', color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 mse, = ax2.plot(pred, loss_mse, color=color, label='L2')
 
-ax1.set_title("MSE (L1) vs MAE (L2)")
+#ax1.set_title("MSE (L1) vs MAE (L2)")
 
 #fig.tight_layout()
 plt.legend(handles=[mse, mae])
@@ -82,10 +82,58 @@ for i in range(len(delta)):
     ax1.plot(pred, losses_huber[i], label = delta[i])
 ax1.set_xlabel('Predictions')
 ax1.set_ylabel('Loss')
-ax1.set_title("Huber Loss/ Smooth MAE Loss")
+#ax1.set_title("Huber Loss/ Smooth MAE Loss")
 ax1.legend(title="$\delta$ values")
 ax1.set_ylim(bottom=-1, top = 15)
 
 print("Exported loss_Huber.svg")
 plt.savefig(fname="../loss_Huber.svg")
 #plt.show()
+
+
+###
+
+def logcosh(true, pred):
+    loss = np.log(np.cosh(pred - true))
+    return np.sum(loss)
+
+fig, ax1 = plt.subplots(1,1, figsize = (7,5))
+
+target = np.repeat(0, 1000) 
+pred = np.arange(-10,10, 0.02)
+
+loss_logcosh = [logcosh(target[i], pred[i]) for i in range(len(pred))]
+
+ax1.plot(pred, loss_logcosh)
+ax1.set_xlabel('Predictions')
+ax1.set_ylabel('Loss')
+print("Exported loss_logcosh.svg")
+plt.savefig(fname="../loss_logcosh.svg")
+
+#plt.show()
+
+def quan(true, pred, theta):
+    loss = np.where(true >= pred, theta*(np.abs(true-pred)), (1-theta)*(np.abs(true-pred)))
+    return np.sum(loss)
+
+
+###
+
+
+fig, ax1 = plt.subplots(1,1, figsize = (7,5))
+
+target = np.repeat(0, 1000) 
+pred = np.arange(-10,10, 0.02)
+
+quantiles = [0.25, 0.5, 0.75, 0.90]
+
+losses_quan = [[quan(target[i], pred[i], q) for i in range(len(pred))] for q in quantiles]
+
+# plot 
+for i in range(len(quantiles)):
+    ax1.plot(pred, losses_quan[i], label = quantiles[i])
+ax1.set_xlabel('Predictions')
+ax1.set_ylabel('Loss')
+ax1.legend(title="Quantiles ($\gamma$)")
+print("Exported loss_quant.svg")
+plt.savefig(fname="../loss_quant.svg")
